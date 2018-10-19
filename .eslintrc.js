24507316e2f262e3a48609c839c9c1db794f2819
Zatorski <lukasz.zatorskii@gmail.com>
2018-10-19 14:59:44 +0200

    Add config for eslint and flow

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+    "parser": "babel-eslint",
+    "plugins": [
+      "react"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "sourceType": "module",
+        "ecmaFeatures": {
+          "arrowFunctions": true,
+          "blockBindings": true,
+          "classes": true,
+          "defaultParams": true,
+          "destructuring": true,
+          "forOf": true,
+          "generators": false,
+          "modules": true,
+          "objectLiteralComputedProperties": true,
+          "objectLiteralDuplicateProperties": false,
+          "objectLiteralShorthandMethods": true,
+          "objectLiteralShorthandProperties": true,
+          "spread": true,
+          "superInFunctions": true,
+          "templateStrings": true,
+          "restParams": true,
+          "jsx": true
+        }
+    },
+    "extends": [
+      "airbnb"
+    ],
+    "settings": {
+      "react": {
+        "createClass": "createReactClass", // Regex for Component Factory to use,
+                                           // default to "createReactClass"
+        "pragma": "React",  // Pragma to use, default to "React"
+        "version": "15.0", // React version, default to the latest React stable release
+        "flowVersion": "0.53" // Flow version
+      },
+      "propWrapperFunctions": [ "forbidExtraProps" ] // The names of any functions used to wrap the
+                                                   // propTypes object, e.g. `forbidExtraProps`.
+                                                   // If this isn't set, any propTypes wrapped in
+                                                   // a function will be skipped.
+  },
+  "rules": {
+    "no-console": 0
+  }
+};
---
+++ app/javascript/components/App/Index.jsx
@@ -0,0 +1,16 @@
+// @flow
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div className="heading">
+          <h1>To-do list</h1>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
